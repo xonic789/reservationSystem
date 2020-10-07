7a0f959c75b152de74d9cc82d3f5a7c4f1064ce7
JinKim <kimjin.dev@gmail.com>
2020-10-07 14:42:00 +0900

    image upload

---
+++ yollowa/src/main/java/com/proj/yollowa/controller/cs/fileuploadCk.java
@@ -0,0 +1,90 @@
+package com.proj.yollowa.controller.cs;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.springframework.stereotype.Controller;
+import org.apache.commons.lang3.StringUtils;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.google.gson.JsonObject;
+
+
+
+@Controller
+public class fileuploadCk {
+	
+	@ResponseBody
+	@RequestMapping(value="/fileupload", method=RequestMethod.POST)
+	public String fileUpload(HttpServletRequest req, HttpServletResponse resp, 
+                 MultipartHttpServletRequest multiFile,@RequestParam MultipartFile upload) throws Exception {
+		JsonObject json = new JsonObject();
+		PrintWriter printWriter = null;
+		OutputStream out = null;
+		MultipartFile file = multiFile.getFile("upload");
+		if(file != null){
+			if(file.getSize() > 0 && StringUtils.isNotBlank(file.getName())){
+				if(file.getContentType().toLowerCase().startsWith("image/")){
+					try{
+						String fileName = file.getName();
+						byte[] bytes = file.getBytes();
+						
+						String uploadPath = req.getServletContext().getRealPath("/resources/img/board/");		
+						/*C:\~~\.metadata\.plugins\org.eclipse.wst.server.core\tmp0\wtpwebapps\*/
+						File uploadFile = new File(uploadPath);
+						if(!uploadFile.exists()){
+							uploadFile.mkdirs();
+						}
+						fileName = UUID.randomUUID().toString();
+						uploadPath = uploadPath + fileName;
+						out = new FileOutputStream(new File(uploadPath));
+                        out.write(bytes);
+                        
+                        printWriter = resp.getWriter();
+                        resp.setContentType("text/html");
+                        String fileUrl = req.getContextPath()+ "/resources/img/board/" + fileName;
+                        
+                        // {"uploaded" : 1, "fileName" : "test.jpg", "url" : "/img/test.jpg"}
+                        json.addProperty("uploaded", 1);
+                        json.addProperty("fileName", fileName);
+                        json.addProperty("url", fileUrl);
+                        
+                        printWriter.println(json);
+                        
+                        System.out.println("uploadPath:"+uploadPath);
+                        System.out.println("fileUrl:"+fileUrl);
+                        
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }finally{
+                        if(out != null){
+                            out.close();
+                        }
+                        if(printWriter != null){
+                            printWriter.close();
+                        }		
+					}
+				}
+			}
+		}
+		return null;
+	}	
+
+}
+
+	
+
