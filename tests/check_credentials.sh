12948b57e99606191830fe6505f4fb7e61e24722
Lee SeungWoo <64122884+xonic789@users.noreply.github.com>
2026-07-13 01:40:56 +0900

    test: validate literal credential placeholders

--- tests/check_credentials.sh
+++ tests/check_credentials.sh
@@ -9,17 +9,22 @@ fail() {
   exit 1
 }
 
+require_placeholder() {
+  local name="$1"
+  grep -qF "value=\"\${$name}\"" "$SPRING_CONFIG" || fail "$name is not externalized"
+}
+
 if rg -n --hidden --glob '!.git/**' --glob '!**/tests/check_credentials.sh' \
   '(AKIA[0-9A-Z]{16}|AIza[0-9A-Za-z_-]{35}|github_pat_|ghp_|BEGIN (RSA |EC |OPENSSH )?PRIVATE KEY)' \
   "$ROOT"; then
   fail "provider credential pattern found"
 fi
 
-grep -qF 'value="${NAVER_CLIENT_ID}"' "$SPRING_CONFIG" || fail "NAVER_CLIENT_ID is not externalized"
-grep -qF 'value="${NAVER_CLIENT_SECRET}"' "$SPRING_CONFIG" || fail "NAVER_CLIENT_SECRET is not externalized"
-grep -qF 'value="${GOOGLE_CLIENT_ID}"' "$SPRING_CONFIG" || fail "GOOGLE_CLIENT_ID is not externalized"
-grep -qF 'value="${GOOGLE_CLIENT_SECRET}"' "$SPRING_CONFIG" || fail "GOOGLE_CLIENT_SECRET is not externalized"
-grep -qF 'key=${GOOGLE_API_KEY}' "$SPRING_CONFIG" || fail "GOOGLE_API_KEY is not externalized"
+require_placeholder NAVER_CLIENT_ID
+require_placeholder NAVER_CLIENT_SECRET
+require_placeholder GOOGLE_CLIENT_ID
+require_placeholder GOOGLE_CLIENT_SECRET
+grep -qF "key=\${GOOGLE_API_KEY}" "$SPRING_CONFIG" || fail "GOOGLE_API_KEY is not externalized"
 
 python3 -c 'import sys, xml.etree.ElementTree as ET; ET.parse(sys.argv[1])' "$SPRING_CONFIG"
 
